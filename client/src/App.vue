b1fcb19ec14c5312e1803430526997307506f1c9
Artem Arefiev <artem.arefiev@uchi.ru>
2019-08-30 18:12:55 +0400

    client: add sidebar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <vs-sidebar
+      static-position
+      default-index="1"
+      color="primary"
+      class="sidebarx"
+      spacer>
+      <vs-sidebar-item to="/addData" index="1">
+        Add Data
+      </vs-sidebar-item>
+      <vs-sidebar-item to="/logs" index="2">
+        Logs
+      </vs-sidebar-item>
+      <vs-sidebar-item to="/data" index="3">
+        Data
+      </vs-sidebar-item>
+    </vs-sidebar>
     <router-view/>
   </div>
 </template>
@@ -18,6 +33,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'vuesax/dist/vuesax.css';
 import Vue from 'vue';
+import Vuesax from 'vuesax';
 import App from './App';
 import router from './router';
 
+Vue.use(Vuesax);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
