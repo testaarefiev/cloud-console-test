95fbfc95c87e2ae0cb7d7e0a6d34ec155b2bbf5d
Artem Arefiev <artem.arefiev@uchi.ru>
2019-09-03 15:54:40 +0400

    server: add cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const redis = require('redis');
 
 const config = require('../config');
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+};
 
 const app = express();
 const redisClient = redis.createClient(config.redis.uri);
@@ -15,8 +20,8 @@ app.use(cookieParser());
 app.use(express.static(config.staticDirectory));
 
 app.use('/', require('./routes/index'));
-app.use('/api/logs', require('./routes/api/logs')(redisClient));
-app.use('/api/data', require('./routes/api/data')(redisClient));
+app.use('/api/logs', cors(corsOptions), require('./routes/api/logs')(redisClient));
+app.use('/api/data', cors(corsOptions), require('./routes/api/data')(redisClient));
 app.use(require('./routes/404'));
 
 module.exports = app;
