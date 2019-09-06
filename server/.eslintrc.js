9732c61dc7826f85e8c739adeb30dea7e73eb3bd
Artem Arefiev <artem.arefiev@uchi.ru>
2019-09-06 11:21:26 +0400

    server: add jest, supertest

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'plugin:jest/recommended',
   ],
   globals: {
     Atomics: 'readonly',
@@ -14,6 +15,14 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2015,
   },
+  plugins: [
+    'jest'
+  ],
   rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
 };
