eb6029efe685423a89499ef90128846208da68b4
Artem Arefiev <artem.arefiev@uchi.ru>
2019-09-06 11:21:12 +0400

    server: fix lint errors

--- server/src/routes/404.js
+++ server/src/routes/404.js
@@ -4,17 +4,17 @@ const config = require('../../config');
 
 const router = express.Router();
 
-router.use((req, res, next) => {
+router.use((req, res) => {
   const message = 'Not found';
 
   res.format({
     text: () => res.send(message),
     html: () => res.sendFile(path.join(config.staticDirectory, '404.html')),
     json: () => res.send({ message }),
-    'default': function () {
+    default() {
       // log the request and respond with 406
-      res.status(406).send('Not Acceptable')
-    }
+      res.status(406).send('Not Acceptable');
+    },
   });
 });
 
